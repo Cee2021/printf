2834fae08d712985a9a3082078c4a00f89fd2f94
Cee2021 <ua_05@ymail.com>
2023-09-19 05:52:07 -0700

    Updated

--- get_size.c
+++ get_size.c
@@ -24,7 +24,33 @@ int get_size(const char *format, int *i)
 
 	return (size);
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> d9af8a62d515dc0117b3c47ad8036a36e61df965
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
